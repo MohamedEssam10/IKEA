3a813432b46180c371c752f208028b588e826e74
Mohamed Essam <mo7amed6102003@gmail.com>
2024-10-15 14:17:49 +0300

    Add project files.

---
+++ IKEA.DAL/Data/Configurations/DepartmentConfigurations.cs
@@ -0,0 +1,21 @@
+﻿using IKEA.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IKEA.DAL.Data.Configurations
+{
+    internal class DepartmentConfigurations : IEntityTypeConfiguration<Department>
+    {
+        public void Configure(EntityTypeBuilder<Department> builder)
+        {
+            // HasComputedColumnSql(); Executes GETDATE() every time the recored is modified
+            builder.Property(D => D.LastModifiedOn).HasComputedColumnSql("GETDATE()");
+            builder.Property(d => d.CreatedOn).HasDefaultValueSql("GETDATE()");
+        }
+    }
+}
---
+++ IKEA.PL/Program.cs
@@ -0,0 +1,47 @@
+using IKEA.DAL.Data;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace IKEA.PL
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+
+            // Apply Dependency Injection to DbContext
+            builder.Services.AddDbContext<ApplicationDbContext>((optionsbuilder) =>
+            {
+                optionsbuilder.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
